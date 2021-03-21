8c2a3e357431989e5dc315fa1b36b597b7850829
kwhua <310697723@qq.com>
2021-03-21 20:32:41 +0800

    add test2

---
+++ src/test/java/com/huaqi/demo/test/Test2.java
@@ -0,0 +1,11 @@
+package com.huaqi.demo.test;
+
+/**
+ * @Author kwhua
+ * @Date 2021/3/21 20:32
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("test2");
+    }
+}
