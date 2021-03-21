5ea4457286752fe4d371e17cfcbe1557ef7e5a62
kwhua <310697723@qq.com>
2021-03-21 16:07:51 +0800

    add sysout 33333

--- src/test/java/com/huaqi/demo/test/MainTest.java
+++ src/test/java/com/huaqi/demo/test/MainTest.java
@@ -8,6 +8,6 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("111111");
         System.out.println("222222");
-
+        System.out.println("333333");
     }
 }
