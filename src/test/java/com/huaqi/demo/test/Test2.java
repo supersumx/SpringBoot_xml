6c15d81e6c2708e95fe290780f565912e890a2a1
supersum <54518706+supersumx@users.noreply.github.com>
2021-03-22 13:40:08 +0800

    Update Test2.java

--- src/test/java/com/huaqi/demo/test/Test2.java
+++ src/test/java/com/huaqi/demo/test/Test2.java
@@ -8,5 +8,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("test2");
         System.out.println("test3");
+        System.out.println("test4");
     }
 }
